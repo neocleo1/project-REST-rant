61b739d76652c81aa30ddaa3b327debf076fee3a
Shannon Stephenson <stephenson.shannon29@gmail.com>
2022-04-11 23:29:44 -0400

    all pages working, added star rating and delete comments

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -3,95 +3,130 @@ const Def = require('../default')
 
 function show(data) {
   let comments = <h3 className="inactive">No comments yet!</h3>
+  let rating = <h3 className="inactive">Not yet rated</h3>
+
   if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars = ''
+    for (let i = 0; i < averageRating; i++) {
+      stars += '⭐️'
+    }
+    rating = <h3>{stars} stars</h3>
+
     comments = data.place.comments.map((c) => {
       return (
-        <div className="border">
+        <div className="border col-sm-4">
           <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
           <h4>{c.content}</h4>
           <h3>
             <stong>- {c.author}</stong>
           </h3>
           <h4>Rating: {c.stars}</h4>
+          <form
+            method="POST"
+            action={`/places/${data.place.id}/comment/${c.id}?_method=DELETE`}
+          >
+            <input
+              type="submit"
+              className="btn btn-danger"
+              value="Delete Comment"
+            />
+          </form>
         </div>
       )
     })
   }
-
   return (
     <Def>
       <main>
-        <img src={data.place.pic} alt={data.place.name} />
-        <h3>
-          Located in {data.place.city}, {data.place.state}
-        </h3>
-        <h1>{data.place.name}</h1>
-        <h2>Rating</h2>
-        <p>Not Rated</p>
-        <h2>Description</h2>
-        <h3>{data.place.showEstablished()}</h3>
-        <h4>Serving {data.place.cuisines}</h4>
-        <h2>Comments</h2>
-        {comments}
+        <div className="row">
+          <div className="col-sm-6">
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+          <div className="col-sm-6">
+            <h1>{data.place.name}</h1>
+            <h2>Rating</h2>
+            <h2>{rating}</h2>
+            <h2>Description</h2>
+            <h3>{data.place.showEstablished()}</h3>
+            <h4> Serving {data.place.cuisines}</h4>
+            <br />
+            <hr />
+            <h2>Comments</h2>
+            <p> {comments} </p>
 
-        <form method="POST" action="/places/${data.place.id}/comment">
-          <div className="form-group">
-            <label htmlFor="name">Author</label>
-            <input
-              className="form-control"
-              id="author"
-              name="author"
-              required
-            />
+            <br />
+            <hr />
+            <h2>Leave a Comment!</h2>
+            <form
+              method="POST"
+              action={`/places/${data.place.id}/comment?_method=POST`}
+            >
+              <div className="row">
+                <div className="form-group col-sm-12">
+                  <label htmlFor="content">Content</label>
+                  <textarea
+                    id="content"
+                    name="content"
+                    className="form-control"
+                  ></textarea>
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-group col-sm-4">
+                  <label htmlFor="author">Author</label>
+                  <input id="author" name="author" className="form-control" />
+                </div>
+                <div className="form-group col-sm-4">
+                  <label htmlFor="stars">Star Rating</label>
+                  <input
+                    type="range"
+                    step="0.5"
+                    min="1"
+                    max="5"
+                    id="stars"
+                    name="stars"
+                    className="form-control"
+                  />
+                </div>{' '}
+                <div className="form-group col-sm-2">
+                  <label htmlFor="rant">Rant?</label>
+                  <input
+                    type="checkbox"
+                    id="rant"
+                    name="rant"
+                    className="form-control"
+                  />
+                </div>
+              </div>
+              <input
+                type="submit"
+                className="btn btn-primary"
+                value="Add Comment"
+              />
+            </form>
+            <a
+              href={`/places/${data.place.id}/edit`}
+              className="btn btn-warning"
+            >
+              Edit
+            </a>
+            <form
+              method="POST"
+              action={`/places/${data.place.id}?_method=DELETE`}
+            >
+              <button type="submit" className="btn btn-danger">
+                Delete
+              </button>
+            </form>
           </div>
-          <div className="form-group">
-            <label htmlFor="pic">Content</label>
-            <textarea
-              className="form-control"
-              id="content"
-              name="content"
-            ></textarea>
-          </div>
-          <div className="form-group">
-            <label htmlFor="city">Star Rating</label>
-            <input
-              type="range"
-              className="form-control"
-              id="stars"
-              name="stars"
-              step="0.5"
-              min="1"
-              max="10"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="city">Rant</label>
-            <input
-              type="checkbox"
-              className="form-control"
-              id="rant"
-              name="rant"
-            />
-          </div>
-        </form>
-
-        <form
-          method="POST"
-          action={`/places/${data.place.id}/comment?_method=POST`}
-        >
-          <button type="submit" className="btn btn-danger">
-            Add Comment
-          </button>
-        </form>
-
-        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-          Edit
-        </a>
-        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-          <button type="submit" className="btn btn-danger">
-            Delete
-          </button>
-        </form>
+        </div>
       </main>
     </Def>
   )
